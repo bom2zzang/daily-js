ea51c14829b52abed80ed2739cf03f66f9097e63
bom2zzang <32053037+bom2zzang@users.noreply.github.com>
2021-01-05 21:52:39 +0900

    Update 210105.js

--- 210105.js
+++ 210105.js
@@ -1,5 +1,5 @@
 /**
-  2021.01.05 
+  2021.01.05 (01)
   완주하지 못한 선수
 */
 
@@ -13,5 +13,35 @@ function solution(participant, completion) {
         return e != completion[i];
     });
         
+    return answer;
+}
+
+
+
+/**
+  (02)
+  위장 
+  :종류별 수는 구했지만 여기서 어떻게 계산해야 하는지 모르겠다. 
+   더 고민해보기.
+  
+*/
+function solution(clothes) {
+    var answer = 0;
+    var obj = new Object();
+    
+    var sortItems = clothes.map((item)=>{
+        
+        var gb = item[1];
+        
+        if(obj[gb]!=null){
+            obj[gb] = obj[gb] + 1;
+        }else{
+            obj[gb] = 1;
+        }
+       
+        
+    });
+    
+    console.log(obj);  
     return answer;
 }
