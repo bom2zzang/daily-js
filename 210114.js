ffcc717c76a8a887c2433cc9c01c4efb7606a8f2
bom2zzang <32053037+bom2zzang@users.noreply.github.com>
2021-01-14 22:13:38 +0900

    Update 210114.js

--- 210114.js
+++ 210114.js
@@ -1,7 +1,7 @@
 /**
     다리를 지나는 트럭
     testcase통과
-    채점 탈락쓰 ㅠ
+    채점 통과!
 
 */
 
@@ -11,29 +11,36 @@ function solution(bridge_length, weight, truck_weights) {
     let bridge_truck = [];
     
     while(true){
-        answer +=1;
-               
-        if(bridge_truck.length == 0){
-            bridge_truck.push(truck_weights[truck_weights.length-length]);   
-            length -= 1;
-        }else{
-            
-            if(length==0){
-                const cnt = bridge_truck.findIndex((item)=>{
-                    return item>0;
-                });                
-                answer += bridge_length-cnt-1;
-                break;
-                
-                
-            }else if(bridge_truck[0]+truck_weights[truck_weights.length-length] > weight){
-                bridge_truck.unshift(0);
-                bridge_truck = bridge_truck.slice(0,bridge_length);
-            }else{
-                bridge_truck.unshift(truck_weights[truck_weights.length-length]);
-                bridge_truck = bridge_truck.slice(0,bridge_length);
+        
+        
+       if(length==0){
+            const cnt = bridge_truck.findIndex((item)=>{
+                return item>0;
+            });                
+            answer += bridge_length-cnt;
+            break; 
+           
+       }else{        
+
+            answer +=1;
+            if(bridge_truck.length == 0){
+                bridge_truck.push(truck_weights[truck_weights.length-length]);   
                 length -= 1;
-            }            
+            }else{
+                
+                const sum_weight = bridge_truck.slice(0,bridge_length-1).reduce((sum,item)=>{
+                        return sum + item; 
+                },0);
+
+                if(sum_weight+truck_weights[truck_weights.length-length] > weight){
+                    bridge_truck.unshift(0);
+                    bridge_truck = bridge_truck.slice(0,bridge_length);
+                }else{
+                    bridge_truck.unshift(truck_weights[truck_weights.length-length]);
+                    bridge_truck = bridge_truck.slice(0,bridge_length);
+                    length -= 1;
+                }            
+            }
         }
          
     }
